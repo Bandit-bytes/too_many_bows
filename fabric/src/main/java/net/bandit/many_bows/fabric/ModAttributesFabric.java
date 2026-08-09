baecde298ad4984b4d7cf1d967f6f49876a19d76
Bandit Bytes <banditheeler2020@gmail.com>
2026-08-08 20:02:47 -0400

    fix fabric 26.2

--- fabric/src/main/java/net/bandit/many_bows/fabric/ModAttributesFabric.java
+++ fabric/src/main/java/net/bandit/many_bows/fabric/ModAttributesFabric.java
@@ -4,13 +4,14 @@ import net.bandit.many_bows.registry.AttributesRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntityTypes;
 import net.minecraft.world.entity.player.Player;
 
 public class ModAttributesFabric {
 
     public static void init() {
         FabricDefaultAttributeRegistry.register(
-                EntityType.PLAYER,
+                EntityTypes.PLAYER,
                 Player.createAttributes()
                         .add(BuiltInRegistries.ATTRIBUTE.wrapAsHolder(AttributesRegistry.BOW_DRAW_SPEED.get()), 1.0D)
                         .add(BuiltInRegistries.ATTRIBUTE.wrapAsHolder(AttributesRegistry.BOW_DAMAGE.get()), 1.0D)
